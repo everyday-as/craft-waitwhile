3a5c14f614accbd598abedabb111192b9b575383
Marcuz <marcuz1111@gmail.com>
2018-09-06 12:52:40 +0200

    translations for phone number must include land code

--- src/translations/en/everyday-waitwhile.php
+++ src/translations/en/everyday-waitwhile.php
@@ -4,4 +4,5 @@ return [
     'name' => 'Full name',
     'phone' => 'Phone number',
     'email' => 'Email address',
+    'phone_landcode_required' => 'Phone number must include land code',
 ];
--- src/translations/nb/everyday-waitwhile.php
+++ src/translations/nb/everyday-waitwhile.php
@@ -4,4 +4,5 @@ return [
     'name' => 'Navn',
     'phone' => 'Telefonnummer',
     'email' => 'Epostadresse',
+    'phone_landcode_required' => 'Telefonnummeret må inkludere landkode',
 ];
